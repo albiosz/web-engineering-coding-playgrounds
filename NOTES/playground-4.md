49851c134958349861da9278ec57e15bdd495dcd
albiosz <szulcalbert@gmail.com>
2025-11-30 14:20:49 +0100

    Migrate the whole body of the page to React

--- NOTES/playground-4.md
+++ NOTES/playground-4.md
@@ -2,3 +2,11 @@
 
 # 1. Use React for the comment web component
 - I have decided to firstly just migrate the `<comment-section />` custom web component to React and embed it in the DOM
+
+# 2. Migrate the whole body into React
+- Next Step was to move all the html from index.html to App.tsx
+
+1. I copied the HTML into the return expression.
+2. Adjusted the HTML according to JSX format.
+
+ 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { initSearchHighlighter } from './searchHighlighter.ts';
 import { fetchBearData } from './bearDataFetcher.ts';
-import { CommentSection } from './components/comment-section/CommentSection.tsx';
+
+import { App } from './App.tsx';
 
 initSearchHighlighter();
 fetchBearData();
 
 // Mount React component
-const commentSectionRoot = document.getElementById('comment-section-root');
-if (commentSectionRoot) {
-  const root = createRoot(commentSectionRoot);
-  root.render(<CommentSection />);
+const rootElement = document.getElementById('app');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
 }
