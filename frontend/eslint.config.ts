6ee3807a3b3528d5773d316140b134bbef17e5b9
albiosz <szulcalbert@gmail.com>
2025-12-29 21:04:04 +0100

    Move all the files to the frontend directory

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from 'eslint/config';
 export default defineConfig([
   {
     // ...love, // when I try to use the love config, I get an error after error that some plugins could not be found
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,tsx}'],
     // plugins: { js, 'eslint-comments': eslintComments },
     plugins: { js },
     extends: ['js/recommended'],
